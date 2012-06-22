c7b7078ec43295a1f94379f1300af33fd3d2155f
Ron 'Ziroby' Romero <ziroby@gmail.com>
2012-06-21 20:03:14 -0500

    Add a map function...
    That does nothing.

---
+++ spec/map.spec.js
@@ -0,0 +1,10 @@
+/*
+ vim:sw=2:ts=2:et
+*/
+var TaleOfTwoMongos = require('../taleoftwomongos');
+
+describe("The map function for counting words", function() {
+  it("should exist", function() {
+    expect(TaleOfTwoMongos.map).toBeDefined();
+  });
+});
