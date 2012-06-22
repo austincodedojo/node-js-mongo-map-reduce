a44167dd175835d13f9b578d593ba7b0d2698bbd
Ron 'Ziroby' Romero <ziroby@gmail.com>
2012-06-21 21:35:23 -0500

    one word done.

--- spec/map.spec.js
+++ spec/map.spec.js
@@ -3,8 +3,72 @@
 */
 var TaleOfTwoMongos = require('../taleoftwomongos');
 
+global.emitCall = {
+} ;
+
+global.emit = function(key, value) {
+  emitCall.callCount++;
+
+  if (typeof emitCall.map[key] === 'undefined') {
+    return emitCall.map[key] = [value];
+  }
+
+  return emitCall.map[key] = emitCall.map[key].concat(value);
+}
+
 describe("The map function for counting words", function() {
+  var context ;
+
+  beforeEach(function() {
+    emitCall = {
+      callCount: 0, 
+      map: {}
+    };
+
+    context = {
+      line: "",
+    };
+  });
+  
   it("should exist", function() {
     expect(TaleOfTwoMongos.map).toBeDefined();
   });
+
+  it("should emit nothing for an empty string", function() {
+
+    TaleOfTwoMongos.map.apply(context);
+    
+    expect(emitCall.callCount).toBe(0);
+  });
+
+  it("should emit 1 for \"the\"", function() {
+    context.line = "the";
+
+    TaleOfTwoMongos.map.apply(context);
+
+    expect(emitCall.callCount).toBe(1);
+    expect(emitCall.map["the"]).toBeDefined();
+    expect(emitCall.map["the"]).toEqual([1]);
+  });
+
+  it("should emit 1 for \"these\"", function() {
+    context.line = "these";
+
+    TaleOfTwoMongos.map.apply(context);
+
+    expect(emitCall.callCount).toBe(1);
+    expect(emitCall.map["these"]).toBeDefined();
+    expect(emitCall.map["these"]).toEqual([1]);
+  });
+
+  it("should emit  for \"the the\"", function() {
+    context.line = "the the";
+
+    TaleOfTwoMongos.map.apply(context);
+
+    expect(emitCall.callCount).toBe(2);
+    expect(emitCall.map["the"]).toBeDefined();
+    expect(emitCall.map["the"]).toEqual([1,1]);
+  });
+
 });
--- taleoftwomongos.js
+++ taleoftwomongos.js
@@ -1,2 +1,7 @@
 exports.map = function() {
+  var words = this.line.split(" ");
+  
+  if (this.line.length > 0) {
+    emit(this.line, 1)
+}
 }
